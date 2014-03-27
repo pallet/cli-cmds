59dc7134bb6dc49801e1da2af7caf4cb46af1349
Hugo Duncan <hugo@hugoduncan.org>
2014-03-26 21:53:15 -0400

    Fix project url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject com.palletops/cli-cmds "0.1.1-SNAPSHOT"
   :description "Library for writing command line interfaces"
-  :url "https://githubs.com/pallet/cli-cmds"
+  :url "https://github.com/pallet/cli-cmds"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
